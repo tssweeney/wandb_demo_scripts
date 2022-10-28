4934ec2db0b0d6a24f1f83d19cb217607494dd09
Tim <tim.s.sweeney@gmail.com>
2022-10-27 21:15:04 -0700

    added model evaluator

--- model_registry_example/model_evaluator.py
+++ model_registry_example/model_evaluator.py
@@ -9,33 +9,45 @@ import wandb
 import util
 import argparse
 
-entity              = "timssweeney"
-project             = "hackweek_2022_post_kyle"
-model_use_case_id   = "mnist"
-job_type            = "evaluator"
+entity = "timssweeney"
+project = "hackweek_2022_post_kyle"
+model_use_case_id = "mnist"
+job_type = "evaluator"
 
 settings = wandb.Settings(enable_job_creation=True)
 
 # First, we launch a run which registers this workload with WandB.
-run = wandb.init(entity="auto-driver", project=project, job_type=job_type, settings=settings, config={
-    "model_artifact": f"wandb-artifact://{entity}/{project}/{model_use_case_id}_candidates:latest"
-})
+run = wandb.init(
+    entity=entity,
+    project=project,
+    job_type=job_type,
+    settings=settings,
+    config={
+        "model_artifact": f"wandb-artifact://{entity}/{project}/{model_use_case_id}_candidates:latest"
+    },
+)
 
 # Then we fetch the latest evaluation set.
 x_eval, y_eval, dataset = util.download_eval_dataset_from_wb(model_use_case_id)
 
 # Next we fetch the new candidate models for this use case
-metric=f"{dataset.name}-ce_loss"
+metric = f"{dataset.name}-ce_loss"
 # candidates = util.get_new_model_candidates_from_wb(project, model_use_case_id, metric)
 
 # Evaluate the models and save their metrics to wb.
 # for model in candidates:
 model = run.config["model_artifact"]
 score = util.evaluate_model(model, x_eval, y_eval)
-# print(model._api.entity)
+
+current_production = wandb.use_artifact(
+    "{entity}/{project}/{model_use_case_id}_candidates:production"
+)
+if score < current_production.metadata[metric] or "demo" in current_production.aliases:
+    model.aliases.append("production")
+
 util.save_metric_to_model_in_wb(model, metric, score)
 
-# TODO: link model to challengers
-run.link_artifact(model, "auto-driver/model-registry/Challengers")
+# # TODO: link model to challengers
+# run.link_artifact(model, "auto-driver/model-registry/Challengers")
 
 run.finish()
