4b8c03a4991f1d830e19a30a05f29fbb55afa05a
Vishwaesh Rajiv <vrajiv@Vish-Rajiv-C02CV4R1MD6R.local>
2022-10-27 20:57:36 -0700

    dummy evaluate job for demo

---
+++ model_registry_example/dummy_evaluate.py
@@ -0,0 +1,21 @@
+import wandb
+import random
+import string
+
+def func():
+    settings = wandb.Settings(enable_job_creation=True)
+    run = wandb.init(project="model-registry", entity="vrajiv", job_type="dummy_evaluator", settings=settings)
+
+    # generate random metric name
+    letters = string.ascii_lowercase
+    random_string = ''.join(random.choice(letters) for i in range(5)) 
+
+    for _ in range(5):
+        run.log({random_string: random.random()})
+
+    run.log_code()
+    run.finish()
+
+
+if __name__ == '__main__':
+    func()
